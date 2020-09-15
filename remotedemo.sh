b84ad19b42b7f4701d26caf4d3f473acbf4a7f58
Mark Menger <m.menger@f5.com>
2020-09-15 16:05:28 +0000

    include scripted load and zap attack

--- remotedemo.sh
+++ remotedemo.sh
@@ -1,7 +1,9 @@
-if [! -d ansible-uber-demo]; then
+if [ ! -d "ansible-uber-demo" ]; then
   git clone https://github.com/mjmenger/ansible-uber-demo
 fi
 cp inventory.yml ansible-uber-demo/ansible/inventory.yml
 cd ansible-uber-demo
 ./install-ubuntu-dependencies.sh
 ./deploy.sh
+nohup ./run-load.sh $JUICESHOP0 10  
+./run-attack.sh http://$JUICESHOP0
