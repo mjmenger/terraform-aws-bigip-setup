67126667829938a809d881c15742e367a83cd12d
Mark Menger <m.menger@f5.com>
2020-09-15 16:52:47 +0000

    fix the params for the load and security scripts

--- remotedemo.sh
+++ remotedemo.sh
@@ -5,5 +5,5 @@ cp inventory.yml ansible-uber-demo/ansible/inventory.yml
 cd ansible-uber-demo
 ./install-ubuntu-dependencies.sh
 ./deploy.sh
-nohup ./run-load.sh $JUICESHOP0 10  
-./run-attack.sh http://$JUICESHOP0
+nohup ./run-load.sh $1 10  
+./run-attack.sh http://$1
