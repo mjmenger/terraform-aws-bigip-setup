e5c26f1e772e1898c3016d6b51f673fce1437bce
Mark Menger <m.menger@f5.com>
2020-09-22 15:52:12 +0000

    retrieve demo-magic and include testing

--- demo.sh
+++ demo.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-
 ########################
 # include the magic
 ########################
-. ../demomagic.sh
+if [ ! -f "demo-magic.sh" ]; then
+  wget https://raw.githubusercontent.com/paxtonhare/demo-magic/master/demo-magic.sh >> /dev/null 2>&1
+fi
+. ./demo-magic.sh
 
 TYPE_SPEED=20
 export AWS_PROFILE=sa_admin_access
@@ -22,10 +24,27 @@ export JUICESHOP1=`terraform output --json | jq -r '.juiceshop_ip.value[1]'`
 export GRAFANA0=`terraform output --json | jq -r '.grafana_ip.value[0]'`
 export GRAFANA1=`terraform output --json | jq -r '.grafana_ip.value[1]'`
 
-pei "scp -i $EC2KEYFILE $EC2KEYFILE ubuntu@$JUMPHOSTIP0:~/$EC2KEYNAME.pem"
-pei "scp -i $EC2KEYFILE ./remotedemo.sh ubuntu@$JUMPHOSTIP0:~/remotedemo.sh"
-pei "ssh -i $EC2KEYFILE ubuntu@$JUMPHOSTIP0 ./remotedemo.sh $JUICESHOP0"
+echo 'test the results'
+pei "./runtests.sh"
+PROMPT_TIMEOUT=120
+echo "that probably failed so let's wait for a couple of minutes"
+wait 
+echo "let's test again - press enter when you're ready"
+pe "./runtests.sh"
+PROMPT_TIMEOUT=0
+echo "that should have succeeded - let's test one more time to be sure - press enter when you're ready" 
+pe "./runtests.sh"
+
+
+echo "prepare the environment - copy over the private key"
+pe "scp -o 'StrictHostKeyChecking no' -i $EC2KEYFILE $EC2KEYFILE ubuntu@$JUMPHOSTIP0:~/$EC2KEYNAME.pem"
+echo "prepare the environment - copy over the configuration script"
+pei "scp -o 'StrictHostKeyChecking no' -i $EC2KEYFILE ./remotedemo.sh ubuntu@$JUMPHOSTIP0:~/remotedemo.sh"
+echo "prepare the environment - remotely execute the configuration script"
+pei "ssh -o 'StrictHostKeyChecking no' -i $EC2KEYFILE ubuntu@$JUMPHOSTIP0 ./remotedemo.sh $JUICESHOP0"
 
 echo connect to BIG-IP at https://$BIGIPHOST0:$BIGIPMGMTPORT with $BIGIPPASSWORD
 echo Juice Shop http://$JUICESHOP0
 echo Grafana http://$GRAFANA0
+
+pe "terraform destroy -auto-approve"
