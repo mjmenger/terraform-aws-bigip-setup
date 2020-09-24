b623e8e985f272c8ab2c7279c5bcc4b4e9db3e6c
Mark Menger <m.menger@f5.com>
2020-09-24 21:08:47 +0000

    export both big-ips to dotenv file

--- dotenv.tf
+++ dotenv.tf
@@ -1,6 +1,7 @@
 resource "local_file" "dotenv" {
     content = <<EOF
 bigip1=${module.bigip.mgmt_public_ips[0]}
+bigip2=${module.bigip.mgmt_public_ips[1]}
 user=admin
 password=${random_password.password.result}
 EOF
